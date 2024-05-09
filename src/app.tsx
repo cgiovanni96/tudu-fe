ce6266512f8472e6973406be79f7dac210165bce
Giovanni Costa <cgiovanni1996@gmail.com>
2024-05-09 15:25:49 +0200

    :sparkles: mantine basic configuratio

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
@@ -22,7 +24,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <MantineProvider>
+        <RouterProvider router={router} />
+      </MantineProvider>
     </StrictMode>,
   );
 }
--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@mantine/core";
 import { createLazyFileRoute } from "@tanstack/react-router";
 
 const Index = () => {
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      <Button>Click me</Button>
     </div>
   );
 };
