3a6a5e6c13b4e430ca1165abeca7de21c3bc7318
Giovanni Costa <cgiovanni1996@gmail.com>
2024-05-27 15:45:52 +0200

    :sparkles: vite & ts path configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
+import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
 });
